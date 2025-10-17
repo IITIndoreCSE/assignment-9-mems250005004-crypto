010463e3d39cb72ff65d0b0a5db9bf36004c855a
mems250005004-crypto <mems250005004@iiti.ac.in>
2025-10-17 16:35:54 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+struct Color {
+    int red;   
+    int green; 
+    int blue;  
+};
+
+int main() {
+    
+    Color colors[5] = {
+        {100, 150, 200},  
+        {255, 100, 50},   
+        {120, 200, 255},  
+        {50, 80, 110},    
+        {180, 220, 240}   
+    };
+
+    
+    for (int i = 0; i < 3; i++) {
+        colors[i].red = 255 - colors[i].red;
+        colors[i].green = 255 - colors[i].green;
+        colors[i].blue = 255 - colors[i].blue;
+    }
+
+    cout << "New RGB values after inversion for all 5 colors:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Color " << i+1 << ": RGB(" 
+             << colors[i].red << ", " 
+             << colors[i].green << ", " 
+             << colors[i].blue << ")" << endl;
+    }
+
+    return 0;
+}
 
