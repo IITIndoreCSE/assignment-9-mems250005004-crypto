16f77ee8640c184e59fb195ac6b432f552f8e2d8
mems250005004-crypto <mems250005004@iiti.ac.in>
2025-10-17 17:23:26 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,46 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct SalesRecord {
+    string month;
+    float amount;
+};
+
+int main() {
+    SalesRecord year[12];
+
+   
+    for (int i = 0; i < 12; i++) {
+        cout << "Month " << (i + 1) << " Name: ";
+        cin.ignore(); // To ignore any leftover newline character
+        getline(cin, year[i].month); 
+        cout << "Sales Amount: ";
+        cin >> year[i].amount;
+    }
+
+   
+    float maxAmount = year[0].amount;
+    float minAmount = year[0].amount;
+    string maxMonth = year[0].month;
+    string minMonth = year[0].month;
+
+    
+    for (int i = 1; i < 12; i++) {
+        if (year[i].amount > maxAmount) {
+            maxAmount = year[i].amount;
+            maxMonth = year[i].month;
+        }
+        if (year[i].amount < minAmount) {
+            minAmount = year[i].amount;
+            minMonth = year[i].month;
+        }
+    }
+
+    
+    cout << "Month with maximum sales: " << maxMonth << " with amount " << maxAmount << endl;
+    cout << "Month with minimum sales: " << minMonth << " with amount " << minAmount << endl;
+
+    return 0;
+}
